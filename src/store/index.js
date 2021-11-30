01c17cfcfd33ad19cda0a41b4d82b2dc3641eec1
Rahul Kasana <rahulkasana@Rahuls-MacBook-Pro.local>
2021-11-30 16:03:15 +0530

    fixes

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,3 @@
-// import {createStore, applyMiddleware} from 'redux';
-// import thunk from 'redux-thunk';
-// import {composeWithDevTools} from 'redux-devtools-extension';
-// import rootReducer from './rootReducer';
-//
-// const middlewares = [thunk];
-//
-// const store =
-//   process.env.NODE_ENV === 'development'
-//     ? createStore(
-//       rootReducer,
-//       composeWithDevTools(applyMiddleware(...middlewares)),
-//     )
-//     : createStore(rootReducer, applyMiddleware(...middlewares));
-//
-// export default store;
-
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
 
