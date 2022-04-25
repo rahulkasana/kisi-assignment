b3e1b5c5b28da215376eb738f25639957f721f3c
RahulKasana <18752429+rahulkasana@users.noreply.github.com>
2022-04-25 09:30:30 +0530

    ga tag

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import "./App.less";
 import { Provider } from "react-redux";
+import gtag from "ga-gtag";
+import "./App.less";
 import store from "./store";
 import Routes from "./routes";
+import { useEffect } from "react";
 
 function App() {
+  const trackDownloadEvent = () => {
+    gtag("event", "test_rahul", {
+      title: "User on website",
+    });
+  };
+  useEffect(() => {
+    trackDownloadEvent();
+  }, []);
   return (
     <Provider store={store}>
       <Routes />
